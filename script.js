4f4651542409f8a6904a2aa7cee54701978dd5d2
RomanStoleru <stoleru.roman@gmail.com>
2023-05-20 19:47:59 -0500

    add_content

--- script.js
+++ script.js
@@ -18,6 +18,6 @@ document.addEventListener("DOMContentLoaded", function () {
 
         slides[slideIndex - 1].style.display = "block";
 
-        setTimeout(autoSlide, 5000); // Change slide every 2 seconds
+        setTimeout(autoSlide, 5000);
     }
 });
